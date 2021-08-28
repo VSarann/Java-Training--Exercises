d69d2be082fc266f509c4f10f3671ab83f43201a
VSarann <89674787+VSarann@users.noreply.github.com>
2021-08-28 10:01:13 +0530

    Update T1.java

--- T1.java
+++ T1.java
@@ -18,10 +18,10 @@ public class T1 {
 		System.out.println("Size of double: " + (Double.SIZE/8) + " bytes.");
 		
 		System.out.println("\nJava Version: "+System.getProperty("java.version"));
-	    System.out.println("Java Runtime Version: "+System.getProperty("java.runtime.version"));
-	    System.out.println("Java Home: "+System.getProperty("java.home"));
-	    System.out.println("Java Vendor: "+System.getProperty("java.vendor"));
-	    System.out.println("Java Vendor URL: "+System.getProperty("java.vendor.url"));
-	    System.out.println("Java Class Path: "+System.getProperty("java.class.path")+"\n");
+	    	System.out.println("Java Runtime Version: "+System.getProperty("java.runtime.version"));
+	    	System.out.println("Java Home: "+System.getProperty("java.home"));
+	    	System.out.println("Java Vendor: "+System.getProperty("java.vendor"));
+	    	System.out.println("Java Vendor URL: "+System.getProperty("java.vendor.url"));
+	    	System.out.println("Java Class Path: "+System.getProperty("java.class.path")+"\n");
 	}
 }
